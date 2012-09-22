f009aed5ba11b13a43a29f73d9d037fced57e0dc
gefupi <gefupi@gmx.net>
2012-09-22 19:12:39 +0200

    added all products to database initialization.

--- init_db.php
+++ init_db.php
@@ -6,15 +6,46 @@
  */
 function kapi_manager_init_db() {
   _kapi_manager_init_products();
+
+//  _kapi_manager_check_init_db();
 }
 
+
+/*
+ * This function does some database checks after initialization
+ */
+function _kapi_manager_check_init_db() {
+  // TODO
+}
 
 /*
  * This function creates the default values for the products table
  */
 function _kapi_manager_init_products() {
+  // TODO: use english product names
+  $products = array(
+    'Wasser', 'Holz', 'Stein', 'Werkzeug', 'Bienenwachs', 'Brote', 'Flachs',
+    'Getreide', 'Honig', 'Käse', 'Kerzen', 'Kräuter', 'Rüben', 'Bier',
+    'Birnenschnaps', 'Met', 'Kirschwasser', 'Schnaps', 'Wein', 'Bibeln',
+    'Bücher', 'Pamphlete', 'Pergamente', 'Schreibfedern', 'Fische', 'Lampenöl',
+    'Muscheln', 'Perlen', 'Räucherfisch', 'Tran', 'Goldgeschmeide',
+    'Heiligenstatuen', 'Muschelketten', 'Perlenketten', 'Silbergeschmeide',
+    'Kohle', 'Pech', 'Gewürze', 'Salz', 'Seide', 'Weihrauch', 'Boote', 'Eisen',
+    'Fuhrwerke', 'Fässer', 'Kandierter Apfel', 'Lebkuchen', 'Mehl', 'Sirup',
+    'Süßes Brot', 'Äpfel', 'Beeren', 'Birnen', 'Kirschen', 'Nüsse', 'Trauben',
+    'Gänsebraten', 'Pökelfleisch', 'Trockenfleisch', 'Wildbret', 'Bänke',
+    'Regale', 'Schränke', 'Stühle', 'Tische', 'Eisenerz', 'Gold', 'Salpeter',
+    'Silber', 'Eier', 'Gänse', 'Hühner', 'Milch', 'Pferde', 'Schweine', 'Wild',
+    'Wolle', 'Ziegen', 'Arkebusen', 'Hellebarde', 'Lederwams', 'Rüstungen',
+    'Schild', 'Schlachtrösser', 'Schwert', 'Daunenbetten', 'Gewänder',
+    'Kleidung', 'Leder', 'Netze', 'Schuhe', 'Teppiche', 'Tuch',
+  );
   $table = 'kapi_manager_products';
-  $record = new stdClass();
-  $record->name = t('water');
-  drupal_write_record($table, $record);
+  foreach ($products as $product) {
+    $record = new stdClass();
+    // TODO use translation function
+    // $record->name = t($product);
+    $record->name = $product;
+    drupal_write_record($table, $record);
+  }
 }
